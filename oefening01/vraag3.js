ff5d1844948d7de6619226942c11ef4111950d78
Stefaan De Cock <stefaan.decock@hogent.be>
2022-05-15 16:56:03 +0200

    Typo

--- oefening01/vraag3.js
+++ oefening01/vraag3.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 /**
  * ===== Vraag 3 =====
  * Details 3.1 tem 3.4: zie hieronder in opgave
  **/
 
 const actor1 = {
-  name: 'Halle Berry',
-  movies: ['Kings', 'Cloud Atlas', 'Kidnap', 'Movie 43']
+  name: "Halle Berry",
+  movies: ["Kings", "Cloud Atlas", "Kidnap", "Movie 43"],
 };
 
 const actor2 = {
-  name: 'Shah Rukh Khan',
-  movies: ['Tutak Tutak Tutiya', 'Jab Tak Hai Jaan']
+  name: "Shah Rukh Khan",
+  movies: ["Tutak Tutak Tutiya", "Jab Tak Hai Jaan"],
 };
 
 const actor3 = {
   name: "Lupita Nyong'o",
-  movies: ['Black Panther', 'The Jungle book', 'Black is King']
+  movies: ["Black Panther", "The Jungle book", "Black is King"],
 };
 
-const extraMovies = ['The 355', 'Us', 'Queen of Katwe'];
+const extraMovies = ["The 355", "Us", "Queen of Katwe"];
 
 function printMovies(actor) {
-  console.log(`${actor.name} starred in: ${actor.movies.join(', ')}`);
+  console.log(`${actor.name} starred in: ${actor.movies.join(", ")}`);
 }
 
 // 3.1 Wijzig de movies van actor1 zodat enkel nog movies die niet starten met de letter 'K' overblijven
@@ -38,11 +38,11 @@ function printMovies(actor) {
 // Schrijf hiervoor 1 statement
 /* 3.3 Oplossing */
 
-// 3.4 Schrijf een functie applyCallbackToObjects.
+// 3.4 Schrijf een functie applyCallbackToActors.
 // De functie moet je kunnen aanroepen met (zie ook voorbeeld uitvoer)
 //   - als eerste argument een callback function
 //   - als volgende argumenten een willekeurig aantal actor objecten
-// In de functie applyCallbackToObjects wordt voor elk actor-object de callback
+// In de functie applyCallbackToActors wordt voor elk actor-object de callback
 // aangeroepen, bij deze aanroep wordt het actor-object als argument doorgegeven
 /* 3.4 Oplossing */
 
